baab4dc90995159f3ba85a6f4a2e91c3cb59408d
Sin <sin@email.com>
2019-09-18 19:23:30 -0300

    Added deploy foolder for github pages.

---
+++ deploy/bootstrap/footer.js
@@ -0,0 +1,21 @@
+/*global React,widgetRegister,bundleRegister,domApi */
+bundleRegister().set('./bootstrap/footer.js', function (props){
+   var e = React.createElement;
+   var newProps = Object.assign({
+      className: 'Footer'
+   }, props);
+   
+   var loader = domApi.WidgetLoader;
+   
+   var widget = loader({
+       file: './widgets/banner.js',
+       css: './widgets/banner.css',
+       widgetRegister: widgetRegister(),
+       loadingRender: e('span', {
+        className: 'loader'
+       }, null)
+   });
+   
+   var element = e('div', newProps, props.title, widget, widget, widget);
+   return element;
+});
