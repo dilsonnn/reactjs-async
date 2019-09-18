24a42e5fd89022caa5c220fe81f028697f926e28
Sin <sin@email.com>
2019-09-18 17:51:40 -0300

    Moved files to src folder.

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+var applicationRoutes = {
+  '/': {
+	 render: 'Index'
+  },
+  '/pages/transfer': {
+	  url: 'pages/transfer/transfer.js'
+  },
+  '/pages/transfer/review': {
+	  url: 'pages/transfer/transferReview.js'
+  },
+  '/pages/transfer/complete': {
+	  url: 'pages/transfer/transferComplete.js'
+  }
+};
