03a3fd955e8ab115a961fff3cbf8c02c67676836
Sin <sin@email.com>
2019-09-19 18:34:53 -0300

    Migrated transfer.js to JSX.

--- src/pages/transfer/transfer.js
+++ src/pages/transfer/transfer.js
@@ -1,25 +1,20 @@
 /*global React,bundleRegister */
 (function(){
     bundleRegister().set('pages/transfer/transfer.js', function(){
-        var e = React.createElement;
-        return e('div', {},
-          e('h1', {}, 'Provide your transaction details:'),
-          e('label', {}, 'Beneficiary'),
-          e('input', { value: 'Adilson'}, null),
-          e('br', {}, null),
-          e('label', {}, 'Amount'),
-          e('input',  { enabled: "false", value: '123.00' }, null),
-          e('br', {}, null),
-          e('a', {
-             href: '#pages/transfer/review'  
-           }, 
-            e('input', {
-             type: 'submit',
-             value: 'Send'
-            }, null)
+        const a = (<div>
+          <h1>Provide your transaction details:</h1>
+          <label>Beneficiary</label>
+          <label>Adilson</label>
+          <br/>
+          <label>Amount</label>
+          <input value="45.54" />
+          <br/>
+          <a href="#pages/transfer/review">
+            <input type="submit" value="Send" />
+          </a>
           
-          )
-        );
+        </div>);
+        return a;
     });
 
 })();
